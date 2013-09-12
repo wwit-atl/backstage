ca1779d37c2daee698254d0f8cc66da3fffff6bf
Donovan Young <dyoung522@gmail.com>
2013-09-11 21:58:38 -0400

    Initial rails install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Backstage::Application.config.secret_key_base = '668a871d957158bf67d54782de932df0fbda1dd212ab92cd59d046880844eb411cab51a39c3640cd68d5450a2d51d99ffd66dc521798a3157da8bebbc1eeafd4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Backstage::Application.config.session_store :cookie_store, key: '_backstage_session'
