694c4497b2c23d29a87d30ac54a85db89e9f4cc6
Donovan C. Young <dyoung522@gmail.com>
2013-09-24 17:49:43 -0400

    update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,55 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
 
-phone = Phone.new(ntype: 'Mobile', number: '4049393709')
-Member.create(
-    username: 'dyoung',
-    password: 'wwit4admin',
-    password_confirmation: 'wwit4admin',
-    email:    'dyoung522@gmail.com',
-    firstname: 'Donovan',
-    lastname:  'Young',
-    phones: [phone]
-)
-Member.first.add_role :admin
+#
+# Create Roles
+#
+
+%w(
+  admin
+  management
+  main_stage
+  apprentice
+  unusual_suspect
+  isp
+  sponsor
+  friend
+).each do |role|
+  Role.create( name: role ) unless Role.where(name: role).exists?
+end
+
+#
+# Create an Admin account
+#
+unless Member.joins(:roles).where('roles.name' => 'admin').exists?
+  phone = Phone.new(ntype: 'Mobile', number: '4049393709')
+  admin = Member.create(
+      username: 'admin',
+      password: 'wwit4admin',
+      password_confirmation: 'wwit4admin',
+      email:    'admin@example.com',
+      firstname: 'Admin',
+      lastname:  'User',
+      phones: [phone]
+  )
+  admin.add_role :admin
+end
+
+#
+# Create Skills
+#
+[
+  [ 'MC', 'Master of Ceremonies', true  ],
+  [ 'HM', 'House Manager',        true  ],
+  [ 'SM', 'Stage Manager',        true  ],
+  [ 'LB', 'Lightboard Operator',  true  ],
+  [ 'SB', 'Soundboard Operator',  true  ],
+  [ 'CS', 'Camera Operator',      true  ],
+  [ 'SS', 'Suggestion Taker',     false ],
+  [ 'BT', 'Bartender',            true  ],
+  [ 'BO', 'Box Office Attendant', true  ],
+].each do |name, desc, training|
+  unless Skill.where(name: name).exists?
+    Skill.create( name: name, description: desc, training?: training )
+  end
+end
