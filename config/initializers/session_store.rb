635a199f482b39cc6370077b6a7abe120bf7c7c1
Donovan C. Young <dyoung522@gmail.com>
2013-10-19 02:02:41 -0400

    add domain: all to session_store to avoid potential heroku problems later on

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Backstage::Application.config.session_store :cookie_store, key: '_backstage_session'
+Backstage::Application.config.session_store :cookie_store, key: '_backstage_session', :domain => :all
