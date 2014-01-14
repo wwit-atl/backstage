25856e74f98f0b452f42cc8fa6c17a4eb4da0617
Donovan C. Young <dyoung522@gmail.com>
2014-01-14 17:42:19 -0500

    style flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,4 +18,13 @@ module ApplicationHelper
     return date.strftime('%m/%d/%Y') if date.respond_to?(:strftime)
     'Invalid Date Format'
   end
+
+  def bs_alert_name(name)
+    case name
+      when :notice then 'alert-info'
+      when :alert  then 'alert-warning'
+      when :error  then 'alert-danger'
+      else "alert-#{name}"
+    end
+  end
 end
