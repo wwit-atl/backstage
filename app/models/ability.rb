add482c504d48a120075873d16e2c1c909e33ad6
Donovan C. Young <dyoung522@gmail.com>
2014-01-27 19:19:50 -0500

    update abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,17 +2,28 @@ class Ability
   include CanCan::Ability
 
   def initialize(member)
-    member ||= Member.new # guest user (not logged in)
-
     # See the wiki for details:
     # https://github.com/ryanb/cancan/wiki/Defining-Abilities
 
+    member ||= Member.new # guest user (not logged in)
+
+    # Guest can't do anything here
+    return if member.new_record?
+
+    can :read, [ Member, Show ]
+    can :update, Member, :id => member.id
+    can [:update, :manage], Conflict, :member_id => member.id
+
     if member.has_role? :admin
       can :manage, :all
-    else
-      can :read, [ Member, Show ]
-      can :update, Member, :id => member.id
-      can [:update, :manage], Conflict, :member_id => member.id
+    end
+
+    if member.has_role? :management
+      can :manage, [ Member, Show ]
+    end
+
+    if member.has_role? :ms
+      can :update, Show
     end
 
   end
