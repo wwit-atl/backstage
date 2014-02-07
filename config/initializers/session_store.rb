da9587e16e30c88ea7c31a869c1f00c635334cf1
Donovan C. Young <dyoung522@gmail.com>
2014-02-07 15:59:05 -0500

    Another potential fix for FF login issue

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-Backstage::Application.config.session_store :cookie_store, key: '_backstage_session', :domain => :all
+#Backstage::Application.config.session_store :cookie_store, key: '_backstage_session', :domain => :all
+Backstage::Application.config.session_store :cookie_store, key: '_backstage_session'
