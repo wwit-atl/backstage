ae3114d16b0a7fd71e34f249270119d4e3f59817
Donovan C. Young <dyoung522@gmail.com>
2014-02-16 02:07:15 -0500

    ?

--- config/environment.rb
+++ config/environment.rb
@@ -2,8 +2,8 @@
 require File.expand_path('../application', __FILE__)
 
 # Load the app's custom environment variables here, so that they are loaded before environments/*.rb
-app_environment_variables = File.join(Rails.root, 'config', 'app_environment_variables.rb')
-load(app_environment_variables) if File.exists?(app_environment_variables)
+#app_environment_variables = File.join(Rails.root, 'config', 'app_environment_variables.rb')
+#load(app_environment_variables) if File.exists?(app_environment_variables)
 
 # Initialize the Rails application.
 Backstage::Application.initialize!
