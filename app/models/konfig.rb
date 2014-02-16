ff5ccfe220c78c80f714e59a5603655ab28712de
Donovan C. Young <dyoung522@gmail.com>
2014-02-16 02:12:37 -0500

    ?

--- app/models/konfig.rb
+++ app/models/konfig.rb
@@ -1,5 +1,5 @@
 # Config is a reserved class, so we use Konfig but refer to Config in the routes
 class Konfig < ActiveRecord::Base
-  #klass = class << self; self; end
-  #Konfig.all.each { |c| klass.send(:define_method, c.name.tableize) { find(c).value.to_i } }
+  klass = class << self; self; end
+  Konfig.all.each { |c| klass.send(:define_method, c.name.tableize) { find(c).value.to_i } }
 end
