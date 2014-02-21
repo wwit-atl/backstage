1a883a40a430c3e1fa2d7dd5191e19ac22f6f6db
Donovan C. Young <dyoung522@gmail.com>
2014-02-21 11:58:45 -0500

    minor formatting changes

--- config/application.rb
+++ config/application.rb
@@ -27,19 +27,19 @@ module Backstage
     # ActionMailer Settings
     config.action_mailer.default_url_options = {  host: ENV['RAILS_HOST'] || 'backstage.wholeworldtheatre.com'  }
     config.action_mailer.smtp_settings = {
-        :domain =>         ENV['MANDRILL_DOMAIN']       || 'wholeworldtheatre.com',
-        :port =>           ENV['MANDRILL_SMTP_PORT']    || '587',
-        :address =>        ENV['MANDRILL_SMTP_ADDRESS'] || 'smtp.mandrillapp.com',
-        :user_name =>      ENV['MANDRILL_USERNAME'],
-        :password =>       ENV['MANDRILL_TESTING'] != 'true' ? ENV['MANDRILL_APIKEY'] : ENV['MANDRILL_TEST_APIKEY'],
+        :domain         => ENV['MANDRILL_DOMAIN']       || 'wholeworldtheatre.com',
+        :port           => ENV['MANDRILL_SMTP_PORT']    || '587',
+        :address        => ENV['MANDRILL_SMTP_ADDRESS'] || 'smtp.mandrillapp.com',
+        :user_name      => ENV['MANDRILL_USERNAME'],
+        :password       => ENV['MANDRILL_TESTING'] != 'true' ? ENV['MANDRILL_APIKEY'] : ENV['MANDRILL_TEST_APIKEY'],
         :authentication => :plain
     }
     config.action_mailer.default_options = {
-        from: 'Laughing Larry <larry@wholeworldtheatre.com>',
-        to: 'Laughing Larry <larry@wholeworldtheatre.com>',
-        reply_to: ENV['DEFAULT_REPLY_TO'] || 'Eric Goins <eric@wholeworldtheatre.com>'
+        :from           => 'Laughing Larry <larry@wholeworldtheatre.com>',
+        :to             => 'Laughing Larry <larry@wholeworldtheatre.com>',
+        :reply_to       => ENV['DEFAULT_REPLY_TO'] || 'Eric Goins <eric@wholeworldtheatre.com>'
     }
     config.action_mailer.delivery_method = :smtp
-    config.action_mailer.perform_deliveries = false unless ENV['NO_EMAIL'].nil?
+    config.action_mailer.perform_deliveries = false if ENV['NO_EMAIL']
   end
 end
