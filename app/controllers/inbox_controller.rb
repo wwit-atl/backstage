00f4a827d34e590361f8800ad6d5290d44767618
Donovan Young <dyoung522@gmail.com>
2015-03-07 15:39:45 -0500

    remove debug statements

--- app/controllers/inbox_controller.rb
+++ app/controllers/inbox_controller.rb
@@ -6,22 +6,14 @@ class InboxController < ApplicationController
   authenticate_with_mandrill_keys! ENV['MANDRILL_WEBHOOK_KEY']
 
   def handle_send(event_payload)
-    puts '>>> Send Event Received'
-
     return if event_payload.nil?
     return unless event_payload['msg'].has_key?('metadata')
 
     message_id = event_payload['msg']['metadata']['message_id']
     return if message_id.nil?
 
-    puts ">>> Received WebHook for message_id: #{message_id}"
-
     message = Message.where(email_message_id: message_id).first
-    if message.nil?
-      puts '>>> No message matches'
-    else
-      puts ">>> Applying to message #{message.id}"
-
+    unless message.nil?
       message.delivered_at = Time.at(event_payload['ts']).to_datetime.utc
       message.save
     end
