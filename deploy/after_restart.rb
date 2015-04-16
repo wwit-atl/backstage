430a6d78c94512469ee31df41c1cd2ff174f13c8
Donovan Young <dyoung522@gmail.com>
2015-04-16 00:44:06 -0400

    another fix for deploy (ugh)

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,8 +1,10 @@
-Chef::Log.info("Starting worker jobs...")
+number_of_workers = new_resource.environment['DJ_WORKERS'] || '2'
+
+Chef::Log.info("Starting #{number_of_workers} Delayed_Job worker(s)...")
 
 execute 'start delayed_job worker(s)' do
   cwd release_path
-  command 'bin/delayed_job -n2 start'
+  command "bin/delayed_job -n#{number_of_workers} start"
   environment new_resource.environment
 end
 
