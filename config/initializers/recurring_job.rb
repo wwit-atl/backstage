93fb046d735327a896fcbf6942526fd360da6271
Donovan Young <dyoung522@gmail.com>
2015-04-20 21:43:54 -0400

    add delayed_job for recurring tasks

---
+++ config/initializers/recurring_job.rb
@@ -0,0 +1,17 @@
+# RecurringJob should log to rails logger
+RecurringJob.logger = Rails.logger
+
+# Require our jobs directory
+Dir['lib/jobs/*.rb'].each { |file| require "jobs/#{File.basename(file, '.rb')}" }
+
+# Schedule Jobs
+hour = 10 # 6 AM EST, based upon UTC
+
+t = Time.now.utc
+t += 1.day if t.hour >= hour
+time = Time.utc(t.year, t.month, t.day, hour)
+Jobs::EmailCrewReminders.schedule_job(first_start_time: time)
+
+t = Time.now.utc
+time = Time.utc(t.year, t.month, t.day, 0)
+Jobs::PurgeAnnouncements.schedule_job(first_start_time: time)
