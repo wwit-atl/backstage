0f5302e2a32024c705b0f4aebfd6b76e860b141f
Donovan Young <dyoung522@gmail.com>
2015-04-26 12:08:25 -0400

    don't schedule recurring jobs in test

--- config/initializers/recurring_job.rb
+++ config/initializers/recurring_job.rb
@@ -4,13 +4,17 @@ RecurringJob.logger = Rails.logger
 # Require our jobs directory
 Dir['lib/jobs/*.rb'].each { |file| require "jobs/#{File.basename(file, '.rb')}" }
 
-# Schedule Jobs
-hour = 10 # 6 AM EST, based upon UTC
+# Don't schedule jobs in test
+if Rails.env != 'test'
+  # Schedule Jobs
+  hour = 10 # 6 AM EST, based upon UTC
 
-t = Time.now.utc
-t += 1.day if t.hour >= hour
-time = Time.utc(t.year, t.month, t.day, hour)
-Jobs::EmailCrewReminders.schedule_job(first_start_time: time)
+  t = Time.now.utc
+  t += 1.day if t.hour >= hour
+  time = Time.utc(t.year, t.month, t.day, hour)
+  Jobs::EmailCrewReminders.schedule_job(first_start_time: time)
 
-time = Time.now.utc.sunday.midnight + 6.hours
-Jobs::PurgeAnnouncements.schedule_job(first_start_time: time)
+  time = Time.now.utc.sunday.midnight + 6.hours
+  Jobs::PurgeAnnouncements.schedule_job(first_start_time: time)
+end
+
