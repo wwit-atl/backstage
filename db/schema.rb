d229777feaa87264eafca6e7333d6687544c72a1
Donovan C. Young <dyoung522@gmail.com>
2016-01-03 22:33:11 -0500

    merged public API

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150407011117) do
+ActiveRecord::Schema.define(version: 20151229175019) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,13 @@ ActiveRecord::Schema.define(version: 20150407011117) do
   end
 
   add_index "addresses", ["member_id"], name: "index_addresses_on_member_id", using: :btree
+
+  create_table "api_keys", force: :cascade do |t|
+    t.string   "access_token"
+    t.string   "note"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "audits", force: :cascade do |t|
     t.string   "ident",      limit: 255
