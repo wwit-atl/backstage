52a23987c543393f8a1d4528bf4fddc35849d004
Donovan C. Young <dyoung522@gmail.com>
2016-01-04 22:24:02 -0500

    Add default_show_capacity to migration

--- db/migrate/20150310044250_add_capacity_to_show.rb
+++ db/migrate/20150310044250_add_capacity_to_show.rb
@@ -1,5 +1,11 @@
 class AddCapacityToShow < ActiveRecord::Migration
   def change
     add_column :shows, :capacity, :integer
+
+    Konfig.create(
+        name:  'DefaultShowCapacity',
+        value: 123,
+        desc:  'The default maximum occupancy for Shows (maximum tickets sold)'
+    )
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -69,17 +69,17 @@ ActiveRecord::Schema.define(version: 20151229175019) do
   add_index "conflicts", ["member_id"], name: "index_conflicts_on_member_id", using: :btree
 
   create_table "delayed_jobs", force: :cascade do |t|
-    t.integer  "priority",   default: 0, null: false
-    t.integer  "attempts",   default: 0, null: false
-    t.text     "handler",                null: false
+    t.integer  "priority",                 default: 0, null: false
+    t.integer  "attempts",                 default: 0, null: false
+    t.text     "handler",                              null: false
     t.text     "last_error"
-    t.datetime "run_at"
-    t.datetime "locked_at"
-    t.datetime "failed_at"
+    t.datetime "run_at",     precision: 6
+    t.datetime "locked_at",  precision: 6
+    t.datetime "failed_at",  precision: 6
     t.string   "locked_by"
     t.string   "queue"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: 6
+    t.datetime "updated_at", precision: 6
   end
 
   add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
