0ef39b15e879a2e568b24241f41862b1097180cc
Donovan C. Young <dyoung522@gmail.com>
2016-01-22 00:35:36 -0500

    Only run Delayed_Job on production server

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,10 +1,15 @@
 number_of_workers = new_resource.environment['DJ_WORKERS'] || '2'
+rails_env         = new_resource.environment['RAILS_ENV']  || 'production'
 
-Chef::Log.info("Starting #{number_of_workers} Delayed_Job worker(s)...")
+if rails_env != 'production'
+  Chef::Log.info("Skipping Delayed_Job startup; RAILS_ENV=#{rails_env}")
+else
+  Chef::Log.info("Starting #{number_of_workers} Delayed_Job worker(s)...")
 
-execute 'start delayed_job worker(s)' do
-  cwd release_path
-  command "bin/delayed_job -n#{number_of_workers} start"
-  environment new_resource.environment
+  execute 'start delayed_job worker(s)' do
+    cwd release_path
+    command "bin/delayed_job -n#{number_of_workers} start"
+    environment new_resource.environment
+  end
 end
 
