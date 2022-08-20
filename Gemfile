950d368b648665beeff605da957a902a60376268
Donovan Young <dyoung522@gmail.com>
2022-08-20 13:44:25 -0400

    Updated to fix issues with more current versions of gems

--- Gemfile
+++ Gemfile
@@ -1,9 +1,9 @@
 source 'https://rubygems.org'
-ruby '2.2.5'
+ruby '2.6.10'
 
 gem 'rails', '~> 4.2'
 gem 'nokogiri', '~> 1.6.5'
-gem 'pg'
+gem 'pg', '~> 0.18'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4'
 gem 'haml', '~> 4.0.0'
@@ -13,6 +13,8 @@ gem 'sass-rails', '>= 3'
 gem 'autoprefixer-rails', '~> 4'
 gem 'will_paginate-bootstrap'
 
+gem "bigdecimal", "~> 1.3.5"
+gem 'psych', '< 4'
 gem 'jquery-rails', '~> 3'
 gem 'jquery-ui-rails'
 gem 'jquery-timepicker-rails'
@@ -22,7 +24,7 @@ gem 'jbuilder', '~> 1.2'
 gem 'bcrypt', '~> 3'
 gem 'acts_as_list'
 gem 'cocoon'
-gem 'devise', '~> 3.0'
+gem 'devise', git: 'https://github.com/plataformatec/devise' , branch: '3-stable'
 gem 'rolify', '~> 3'
 gem 'cancancan'
 gem 'chosen-rails'
@@ -77,9 +79,8 @@ end
 
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
-  gem 'sdoc', require: false
+  gem 'sdoc', '~> 1.0', require: false
 end
 
 # Rails Server
 gem 'unicorn'
-
--- db/schema.rb
+++ db/schema.rb
@@ -26,15 +26,15 @@ ActiveRecord::Schema.define(version: 20151229175019) do
   add_index "actors_shows", ["show_id"], name: "index_actors_shows_on_show_id", using: :btree
 
   create_table "addresses", force: :cascade do |t|
-    t.string   "street1",    limit: 255
-    t.string   "street2",    limit: 255
-    t.string   "city",       limit: 255
-    t.string   "state",      limit: 255
-    t.string   "zip",        limit: 255
-    t.string   "atype",      limit: 255
+    t.string   "street1"
+    t.string   "street2"
+    t.string   "city"
+    t.string   "state"
+    t.string   "zip"
+    t.string   "atype"
     t.integer  "member_id"
-    t.datetime "created_at",             precision: 6
-    t.datetime "updated_at",             precision: 6
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "addresses", ["member_id"], name: "index_addresses_on_member_id", using: :btree
@@ -47,11 +47,11 @@ ActiveRecord::Schema.define(version: 20151229175019) do
   end
 
   create_table "audits", force: :cascade do |t|
-    t.string   "ident",      limit: 255
+    t.string   "ident"
     t.text     "message"
     t.integer  "member_id"
-    t.datetime "created_at",             precision: 6
-    t.datetime "updated_at",             precision: 6
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "audits", ["member_id"], name: "index_audits_on_member_id", using: :btree
@@ -61,35 +61,35 @@ ActiveRecord::Schema.define(version: 20151229175019) do
     t.integer  "month"
     t.integer  "day"
     t.integer  "member_id"
-    t.datetime "created_at", precision: 6
-    t.datetime "updated_at", precision: 6
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.boolean  "lock"
   end
 
   add_index "conflicts", ["member_id"], name: "index_conflicts_on_member_id", using: :btree
 
   create_table "delayed_jobs", force: :cascade do |t|
-    t.integer  "priority",                 default: 0, null: false
-    t.integer  "attempts",                 default: 0, null: false
-    t.text     "handler",                              null: false
+    t.integer  "priority",   default: 0, null: false
+    t.integer  "attempts",   default: 0, null: false
+    t.text     "handler",                null: false
     t.text     "last_error"
-    t.datetime "run_at",     precision: 6
-    t.datetime "locked_at",  precision: 6
-    t.datetime "failed_at",  precision: 6
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
     t.string   "locked_by"
     t.string   "queue"
-    t.datetime "created_at", precision: 6
-    t.datetime "updated_at", precision: 6
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
 
   create_table "friendly_id_slugs", force: :cascade do |t|
-    t.string   "slug",           limit: 255,               null: false
-    t.integer  "sluggable_id",                             null: false
+    t.string   "slug",                      null: false
+    t.integer  "sluggable_id",              null: false
     t.string   "sluggable_type", limit: 50
-    t.string   "scope",          limit: 255
-    t.datetime "created_at",                 precision: 6
+    t.string   "scope"
+    t.datetime "created_at"
   end
 
   add_index "friendly_id_slugs", ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true, using: :btree
@@ -98,46 +98,51 @@ ActiveRecord::Schema.define(version: 20151229175019) do
   add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type", using: :btree
 
   create_table "konfigs", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.string   "value",      limit: 255
-    t.string   "desc",       limit: 255
-    t.datetime "created_at",             precision: 6
-    t.datetime "updated_at",             precision: 6
+    t.string   "name"
+    t.string   "value"
+    t.string   "desc"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
+  add_index "konfigs", ["name"], name: "index_konfigs_on_name", using: :btree
+  add_index "konfigs", ["value"], name: "index_konfigs_on_value", using: :btree
+
   create_table "members", force: :cascade do |t|
-    t.string   "email",                  limit: 255
-    t.string   "lastname",               limit: 255
-    t.string   "firstname",              limit: 255
-    t.string   "sex",                    limit: 255
+    t.string   "email"
+    t.string   "lastname"
+    t.string   "firstname"
+    t.string   "sex"
     t.date     "dob"
-    t.boolean  "active",                                           default: true
-    t.datetime "created_at",                         precision: 6
-    t.datetime "updated_at",                         precision: 6
-    t.string   "encrypted_password",     limit: 255,               default: "",    null: false
-    t.string   "reset_password_token",   limit: 255
-    t.datetime "reset_password_sent_at",             precision: 6
-    t.datetime "remember_created_at",                precision: 6
-    t.integer  "sign_in_count",                                    default: 0,     null: false
-    t.datetime "current_sign_in_at",                 precision: 6
-    t.datetime "last_sign_in_at",                    precision: 6
-    t.string   "current_sign_in_ip",     limit: 255
-    t.string   "last_sign_in_ip",        limit: 255
-    t.string   "confirmation_token",     limit: 255
-    t.datetime "confirmed_at",                       precision: 6
-    t.datetime "confirmation_sent_at",               precision: 6
-    t.string   "unconfirmed_email",      limit: 255
+    t.boolean  "active",                 default: true
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "encrypted_password",     default: "",    null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,     null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
     t.integer  "last_message_id"
-    t.string   "slug",                   limit: 255
+    t.string   "slug"
     t.boolean  "superuser"
-    t.boolean  "conflict_exempt",                                  default: false
+    t.boolean  "conflict_exempt",        default: false
     t.string   "avatar_file_name"
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
   end
 
-  add_index "members", ["email"], name: "index_members_on_email", unique: true, using: :btree
+  add_index "members", ["email"], name: "index_members_on_email", using: :btree
+  add_index "members", ["firstname"], name: "index_members_on_firstname", using: :btree
+  add_index "members", ["lastname"], name: "index_members_on_lastname", using: :btree
   add_index "members", ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true, using: :btree
   add_index "members", ["slug"], name: "index_members_on_slug", unique: true, using: :btree
 
@@ -162,50 +167,52 @@ ActiveRecord::Schema.define(version: 20151229175019) do
   add_index "members_skills", ["skill_id"], name: "index_members_skills_on_skill_id", using: :btree
 
   create_table "messages", force: :cascade do |t|
-    t.string   "subject",          limit: 255
+    t.string   "subject"
     t.text     "message"
     t.integer  "sender_id"
     t.integer  "approver_id"
-    t.datetime "sent_at",                      precision: 6
-    t.datetime "created_at",                   precision: 6
-    t.datetime "updated_at",                   precision: 6
-    t.datetime "delivered_at",                 precision: 6
-    t.string   "email_message_id", limit: 255
-    t.datetime "approved_at",                  precision: 6
+    t.datetime "sent_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.datetime "delivered_at"
+    t.string   "email_message_id"
+    t.datetime "approved_at"
   end
+
+  add_index "messages", ["sent_at"], name: "index_messages_on_sent_at", using: :btree
 
   create_table "notes", force: :cascade do |t|
     t.text     "content"
     t.integer  "member_id"
     t.integer  "notable_id"
-    t.string   "notable_type", limit: 255
-    t.datetime "created_at",               precision: 6
-    t.datetime "updated_at",               precision: 6
+    t.string   "notable_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "notes", ["member_id"], name: "index_notes_on_member_id", using: :btree
   add_index "notes", ["notable_id", "notable_type"], name: "index_notes_on_notable_id_and_notable_type", using: :btree
 
   create_table "phones", force: :cascade do |t|
-    t.string   "number",     limit: 255
-    t.string   "ntype",      limit: 255
+    t.string   "number"
+    t.string   "ntype"
     t.integer  "member_id"
-    t.datetime "created_at",             precision: 6
-    t.datetime "updated_at",             precision: 6
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "phones", ["member_id"], name: "index_phones_on_member_id", using: :btree
 
   create_table "roles", force: :cascade do |t|
-    t.string   "name",          limit: 255
-    t.string   "desc",          limit: 255
+    t.string   "name"
+    t.string   "desc"
     t.integer  "resource_id"
-    t.string   "resource_type", limit: 255
-    t.datetime "created_at",                precision: 6
-    t.datetime "updated_at",                precision: 6
+    t.string   "resource_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.boolean  "cast"
     t.boolean  "crew"
-    t.string   "title",         limit: 255
+    t.string   "title"
     t.boolean  "schedule"
     t.boolean  "cm"
   end
@@ -219,8 +226,8 @@ ActiveRecord::Schema.define(version: 20151229175019) do
     t.text     "suggestion"
     t.integer  "show_id"
     t.integer  "stage_id"
-    t.datetime "created_at", precision: 6
-    t.datetime "updated_at", precision: 6
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "shifts", force: :cascade do |t|
@@ -228,8 +235,8 @@ ActiveRecord::Schema.define(version: 20151229175019) do
     t.integer "skill_id"
     t.integer "member_id"
     t.boolean "training"
-    t.string  "uuid",      limit: 255
-    t.boolean "hidden",                default: false
+    t.string  "uuid"
+    t.boolean "hidden",    default: false
   end
 
   add_index "shifts", ["member_id"], name: "index_shifts_on_member_id", using: :btree
@@ -237,12 +244,12 @@ ActiveRecord::Schema.define(version: 20151229175019) do
   add_index "shifts", ["skill_id"], name: "index_shifts_on_skill_id", using: :btree
 
   create_table "show_templates", force: :cascade do |t|
-    t.string   "name",       limit: 255
+    t.string   "name"
     t.integer  "dow"
     t.time     "showtime"
     t.time     "calltime"
-    t.datetime "created_at",             precision: 6
-    t.datetime "updated_at",             precision: 6
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "group_id"
   end
 
@@ -252,15 +259,15 @@ ActiveRecord::Schema.define(version: 20151229175019) do
   end
 
   create_table "shows", force: :cascade do |t|
-    t.string   "name",            limit: 255
+    t.string   "name"
     t.date     "date"
     t.time     "showtime"
     t.time     "calltime"
-    t.datetime "created_at",                  precision: 6
-    t.datetime "updated_at",                  precision: 6
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "mc_id"
     t.integer  "group_id"
-    t.datetime "casting_sent_at",             precision: 6
+    t.datetime "casting_sent_at"
     t.hstore   "tickets"
     t.integer  "capacity"
   end
@@ -268,22 +275,26 @@ ActiveRecord::Schema.define(version: 20151229175019) do
   add_index "shows", ["date"], name: "index_shows_on_date", using: :btree
 
   create_table "skills", force: :cascade do |t|
-    t.string   "code",        limit: 255
-    t.string   "name",        limit: 255
+    t.string   "code"
+    t.string   "name"
     t.integer  "priority"
     t.text     "description"
     t.boolean  "training"
     t.boolean  "autocrew"
-    t.datetime "created_at",              precision: 6
-    t.datetime "updated_at",              precision: 6
-    t.boolean  "limits",                                default: true
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.boolean  "limits",      default: true
   end
 
+  add_index "skills", ["code"], name: "index_skills_on_code", using: :btree
+  add_index "skills", ["name"], name: "index_skills_on_name", using: :btree
+  add_index "skills", ["priority"], name: "index_skills_on_priority", using: :btree
+
   create_table "stages", force: :cascade do |t|
-    t.string   "code",       limit: 255
-    t.string   "name",       limit: 255
-    t.datetime "created_at",             precision: 6
-    t.datetime "updated_at",             precision: 6
+    t.string   "code"
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
