958bd0f8910b3d454a6b7f6bcb2bebc96106e3e5
Donovan Young <dyoung522@gmail.com>
2022-12-26 10:40:42 -0500

    Update Rails to 4.2.11.3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -92,10 +92,10 @@ class ApplicationController < ActionController::Base
   private
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.for(:sign_up) do |u|
+      devise_parameter_sanitizer.permit(:sign_up) do |u|
         u.permit(:first_name, :last_name, :email, :password, :password_confirmation)
       end
-      devise_parameter_sanitizer.for(:account_update) { |u|
+      devise_parameter_sanitizer.permit(:account_update) { |u|
         u.permit(:password, :password_confirmation, :current_password)
       }
     end
